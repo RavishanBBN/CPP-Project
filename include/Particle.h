17385c86003338f929628b2ecd29c030bb1db428
Nilesh Ravishan <nileshravishanmu@gmail.com>
2025-05-03 13:21:37 +0530

    9

--- include/Particle.h
+++ include/Particle.h
@@ -1,39 +1,48 @@
 #pragma once
-
-#include <vector>
-#include <memory>
 #include <mutex>
-#include <atomic>
 
+/// A single particle in the simulation.
 class Particle {
 public:
-    Particle(double x, double y, double energy, double radius, double max_energy);
+    Particle(double x, double y, double energy,
+             double radius, double maxEnergy);
     ~Particle();
 
+    // --- position --------------------------------------------------
     double getX() const;
     double getY() const;
-    void setPosition(double x, double y);
+    void   setPosition(double x, double y);
 
+    // --- velocity --------------------------------------------------
     double getVX() const;
     double getVY() const;
-    void setVelocity(double vx, double vy);
+    void   setVelocity(double vx, double vy);
 
+    // --- energy ----------------------------------------------------
     double getEnergy() const;
-    void setEnergy(double energy);
-    void addEnergy(double delta);
     double getMaxEnergy() const;
+    void   setEnergy(double e);
+    void   addEnergy(double delta);
 
-    void collide(Particle& other); 
-    bool isColliding(const Particle& other) const;
+    // --- geometry --------------------------------------------------
+    double getRadius() const { return radius; }
+
+    // --- collisions ------------------------------------------------
+    bool   isColliding(const Particle& other) const;
+    void   collide(Particle& other);
+
+    Particle(const Particle&)            = delete;
+    Particle& operator=(const Particle&) = delete;
 
 private:
-    double x, y;
-    double vx, vy;
-    
-    double energy;
-    const double MAX_ENERGY;
-    
-    const double PARTICLE_RADIUS;
-    
-    mutable std::mutex particleMutex;
-}; 
+    mutable std::mutex mtx;     // protects all mutable members
+
+    // state
+    double x{}, y{};
+    double vx{}, vy{};
+    double energy{};
+
+    // constants
+    const double radius;
+    const double maxEnergy;
+};
